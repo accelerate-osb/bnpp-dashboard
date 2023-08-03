40f1797eab2f8720a102c2f6eabbcee3e2f010b3
Zeel Patel <zeel.patel2@ibm.com>
2023-08-03 13:07:30 +0530

    fixed padding

--- src/components/Tile2.js
+++ src/components/Tile2.js
@@ -8,24 +8,26 @@ const Tile2 = ({ darkMode }) => {
     <div className={darkMode ? style.tileContainerDark : style.tileContainer}>
       <p className={style.title}>Need help?</p>
       <br />
-      <p className={style.description}>
-        Submit an IBM Cloud Support Case to our team
-      </p>
-      <br />
-      <div className={style.buttons}>
-        <Button
-          kind="tertiary"
-          className={darkMode ? style.tertialryButtonDark : ""}
-          renderIcon={Launch}
-          onClick={() =>
-            window.open(
-              "https://www.ibm.com/mysupport/s/createrecord/NewCase",
-              "_blank"
-            )
-          }
-        >
-          Support case
-        </Button>
+      <div className={style.content}>
+        <p className={style.description}>
+          Submit an IBM Cloud Support Case to our team
+        </p>
+        <br />
+        <div className={style.buttons}>
+          <Button
+            kind="tertiary"
+            className={darkMode ? style.tertialryButtonDark : ""}
+            renderIcon={Launch}
+            onClick={() =>
+              window.open(
+                "https://www.ibm.com/mysupport/s/createrecord/NewCase",
+                "_blank"
+              )
+            }
+          >
+            Support case
+          </Button>
+        </div>
       </div>
     </div>
   );
