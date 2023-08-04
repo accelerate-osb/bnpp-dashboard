4f83ce60274501fb8cd875302b8a147a5678d220
Zeel Patel <zeel.patel2@ibm.com>
2023-08-04 17:37:12 +0530

    remove padding from content

--- src/components/Tile1.js
+++ src/components/Tile1.js
@@ -9,53 +9,51 @@ const Tile1 = ({ darkMode }) => {
     <div className={darkMode ? style.tileContainerDark : style.tileContainer}>
       <p className={style.title}>Getting Started</p>
       <br />
-      <div className={style.content}>
-        <p className={style.description}>
-          Thank you for subscribing to IBM Cloud SEIM!
-          <br />
-          <br />
-          The IBM Cloud team is setting up your instance and providing access to
-          your user administrator.
-          <br />
-          Access make take up to two business days.
-          <br />
-          <br />
-          If you need to reach out to the IBM Cloud SEIM team sooner, you can
-          email the
-          <Link
-            className={darkMode ? style.linkDark : ""}
-            href="mailto: SaaS-CoE-Balakrishnan-WW-DG@ibm.com"
-          >
-            IBM Cloud SaaS Center of Excellence.
-          </Link>
-          <br />
-          <br />
-          Once your instance is setup and access is granted, you can reach the
-          IBM Cloud SEIM UI dashboard using the button below.
-        </p>
-        <div className={style.buttons}>
-          <Button
-            className={darkMode ? style.primaryButtonDark : style.button}
-            renderIcon={Launch}
-            onClick={() =>
-              window.open("https://console-00885.qradar.ibmcloud.com", "_blank")
-            }
-          >
-            Go to IBM Cloud SEIM Dashboard
-          </Button>
-          <Button
-            className={darkMode ? style.primaryButtonDark : style.button}
-            renderIcon={Launch}
-            onClick={() =>
-              window.open(
-                "https://www.ibm.com/support/knowledgecenter/en/SS42VS_7.5/com.ibm.qradar.doc/c_qradar_gs_using_siem.html",
-                "_blank"
-              )
-            }
-          >
-            Documentation
-          </Button>
-        </div>
+      <p className={style.description}>
+        Thank you for subscribing to IBM Cloud SEIM!
+        <br />
+        <br />
+        The IBM Cloud team is setting up your instance and providing access to
+        your user administrator.
+        <br />
+        Access make take up to two business days.
+        <br />
+        <br />
+        If you need to reach out to the IBM Cloud SEIM team sooner, you can
+        email the
+        <Link
+          className={darkMode ? style.linkDark : ""}
+          href="mailto: SaaS-CoE-Balakrishnan-WW-DG@ibm.com"
+        >
+          IBM Cloud SaaS Center of Excellence.
+        </Link>
+        <br />
+        <br />
+        Once your instance is setup and access is granted, you can reach the IBM
+        Cloud SEIM UI dashboard using the button below.
+      </p>
+      <div className={style.buttons}>
+        <Button
+          className={darkMode ? style.primaryButtonDark : style.button}
+          renderIcon={Launch}
+          onClick={() =>
+            window.open("https://console-00885.qradar.ibmcloud.com", "_blank")
+          }
+        >
+          Go to IBM Cloud SEIM Dashboard
+        </Button>
+        <Button
+          className={darkMode ? style.primaryButtonDark : style.button}
+          renderIcon={Launch}
+          onClick={() =>
+            window.open(
+              "https://www.ibm.com/support/knowledgecenter/en/SS42VS_7.5/com.ibm.qradar.doc/c_qradar_gs_using_siem.html",
+              "_blank"
+            )
+          }
+        >
+          Documentation
+        </Button>
       </div>
     </div>
   );
